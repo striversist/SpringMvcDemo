5a146b90f85e2a1ab42455a7b8a54d6e1065c852
aarontang <aarontang@tencent.com>
2016-03-10 14:20:56 +0800

    [exception]exception & error page（关键：xml中的配置 & @ExceptionHandler）

--- src/main/java/com/demo/controller/StudentController.java
+++ src/main/java/com/demo/controller/StudentController.java
@@ -2,11 +2,13 @@ package com.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.demo.exception.SpringException;
 import com.demo.model.Student;
 
 @Controller
@@ -18,10 +20,20 @@ public class StudentController {
     }
     
     @RequestMapping(value = "/addStudent", method = RequestMethod.POST)
+    @ExceptionHandler({SpringException.class})
     public String addStudent(@ModelAttribute Student student, ModelMap modelMap) {
         modelMap.put("id", student.getId());
         modelMap.put("name", student.getName());
-        modelMap.put("age", student.getAge());
+        
+        if (student.getAge() < 0) {
+            throw new RuntimeException("Given age less than 0");
+        }
+        
+        if (student.getAge() < 10) {
+            throw new SpringException("Given age(" + student.getAge() + ") is too low");
+        } else {
+            modelMap.put("age", student.getAge());
+        }
         
         return "result";
     }
---
+++ src/main/java/com/demo/exception/SpringException.java
@@ -0,0 +1,19 @@
+package com.demo.exception;
+
+public class SpringException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+    private String exceptionMsg;
+
+    public SpringException(String exceptionMsg) {
+        this.exceptionMsg = exceptionMsg;
+    }
+
+    public String getExceptionMsg() {
+        return exceptionMsg;
+    }
+
+    public void setExceptionMsg(String exceptionMsg) {
+        this.exceptionMsg = exceptionMsg;
+    }
+}
